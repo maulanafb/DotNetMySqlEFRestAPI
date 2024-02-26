2876167b524e1541f876d1a66fa84f73259104a7
unknown <gagas112212@gmail.com>
2024-02-26 10:05:00 +0700

    create people repo and interface

---
+++ DotNetMySqlEFRestAPI.Data/Repositories/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using DotNetMySqlEFRestAPI.Data.Models;
+
+namespace DotNetMySqlEFRestAPI.Data.Repositories
+{
+    public interface IPersonRepository
+    {
+        Task<Person> CreatePersonAsync(Person person);
+        Task DeletePersonAsync(Person person);
+        Task<IEnumerable<Person>> GetPeopleAsync();
+        Task<Person> GetPeopleByIdAsync(int id);
+        Task UpdatePersonAsync(Person person);
+    }
+}
---
+++ DotNetMySqlEFRestAPI.Data/Repositories/PersonRepository.cs
@@ -0,0 +1,43 @@
+﻿using DotNetMySqlEFRestAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DotNetMySqlEFRestAPI.Data.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly PersonContext _ctx;
+        public PersonRepository(PersonContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<IEnumerable<Person>> GetPeopleAsync()
+        {
+            var people = await _ctx.People.ToListAsync();
+            return people;
+        }
+
+        public async Task<Person> GetPeopleByIdAsync(int id)
+        {
+            var person = await _ctx.People.FindAsync(id);
+            return person;
+        }
+        public async Task<Person> CreatePersonAsync(Person person)
+        {
+            _ctx.People.Add(person);
+            await _ctx.SaveChangesAsync();
+            return person;
+        }
+
+        public async Task UpdatePersonAsync(Person person)
+        {
+            _ctx.People.Update(person);
+            await _ctx.SaveChangesAsync();
+        }
+        public async Task DeletePersonAsync(Person person)
+        {
+            _ctx.People.Remove(person);
+            await _ctx.SaveChangesAsync();
+        }
+    }
+}
